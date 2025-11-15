294ae848ee52b1b37b0315fe93ee0ad384990bc7
sicortesm <simoncor81@gmail.com>
2025-11-15 10:02:50 -0500

    mod: se crea el test migración

---
+++ homework/src/wordcount.py
@@ -0,0 +1,24 @@
+import os
+
+files_in_input_dir = os.listdir("data/input/")
+files_in_input_dir
+
+
+counter = {}
+for filename in files_in_input_dir:
+    with open("data/input/" + filename) as f:
+        for l in f:
+            for w in l.split():
+                w = w.lower().strip(",.!?")
+                counter[w] = counter.get(w, 0) + 1
+
+
+# create the directory output/ if it doesn't exist
+if not os.path.exists("data/output"):
+    os.makedirs("data/output")
+
+# save the results using tsv format
+with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+    for key, value in counter.items():
+        # write the key and value to the file
+        f.write(f"{key}\t{value}\n")
